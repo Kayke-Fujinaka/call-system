869e97ed9197e4a66a8bc6fb6925aa92ee9cfb81
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-17 09:28:44 -0300

    [FOLDER] SignIn and SignUp pages

--- src/Pages/SignIn/index.js
+++ src/Pages/SignIn/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function App() {
+export default function SignIn() {
   return (
     <div>
-      <h1>TESTE</h1>
+      <h1>Página de Login</h1>
     </div>
   );
 }
---
+++ src/Pages/SignUp/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignUp() {
+  return (
+    <div>
+      <h1>Página de Registro</h1>
+    </div>
+  );
+}
