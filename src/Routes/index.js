f5f55f758bb118f9d2d8e2cd2fbec0315cf1399f
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-17 10:25:07 -0300

    [FEATURE] Dashboard route created

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,11 +4,15 @@ import Route from "./Route";
 import SignIn from "../Pages/SignIn";
 import SignUp from "../Pages/SignUp";
 
+import Dashboard from "../Pages/Dashboard";
+
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/register" component={SignUp} />
+
+      <Route exact path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 }
