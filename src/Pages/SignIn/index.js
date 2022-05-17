74457cf5addcd095cd5e9778d3ab9238c3fadc1a
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-17 17:51:31 -0300

    [STRUCTURING & STYLE] Login page created

--- src/Pages/SignIn/index.js
+++ src/Pages/SignIn/index.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import * as S from "./styles";
 
+const linkStyle = {
+  marginLeft: "0.5rem",
+  textDecoration: "none",
+  color: '#526eff'
+};
+
 export default function SignIn() {
   return (
-    <S.Dive>
-      <h1>Página de Login</h1>
-    </S.Dive>
+    <div className="wrapper">
+      <div className="mainContainer">
+        <S.H1>Faça o seu login</S.H1>
+
+        <S.Form>
+          <input type="text" name="email" placeholder="Email" autocomplete="off" />
+          <input type="password" name="password" placeholder="Senha" />
+          <button type="submit">Entrar</button>
+        </S.Form>
+
+        <p>Ainda não tem uma conta?<Link to="/register" style={linkStyle}>Criar uma conta</Link></p>
+      </div>
+    </div>
   );
 }
--- src/Styles/global.js
+++ src/Styles/global.js
@@ -40,6 +40,31 @@ button {
     cursor: pointer;
 }
 
+.wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color: #1C1E28;
+    color: ${theme.white};
+}
+
+.mainContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    padding: 3.5rem;
+    background-color: #191921;
+    border-radius: 1rem;
+    @media (min-width: 600px) {
+        width: min(60rem, 100%);
+        height: min(55rem, 100%);
+    }
+}
+
 `;
 
 export default GlobalStyle;
