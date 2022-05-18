4712ba5ba4a55bed38da7bfd2f71ebc5dc48622f
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-18 11:00:37 -0300

    [INSTALL] Add Toastify in the project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import AuthProvider from "./Context/auth";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
+
 import GlobalStyle from "./Styles/global";
+
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+      <ToastContainer autoClose={3000} />
         <Routes />
       </BrowserRouter>
       <GlobalStyle />
