1abb5903c30747a24d19c50cd1286ce659c61ab5
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-18 09:25:55 -0300

    [FEATURE] Import firestore in the firebase

--- src/Services/firebaseConnection.js
+++ src/Services/firebaseConnection.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyClzbcrIka_2bFOMQQLFr52BtDs3EbgYzY",
