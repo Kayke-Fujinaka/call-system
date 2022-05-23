8c91ca2102fc1da6bbf30c72ed3189f2c242a273
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-23 08:31:01 -0300

    [STRUCTURING] Customers Page

--- src/Pages/Customers/index.js
+++ src/Pages/Customers/index.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-import * as S from "./styles";
+import Header from "../../Components/Header";
+import Title from "../../Components/Title";
+
+import { FiUser } from "react-icons/fi";
+
+// import * as S from "./styles";
 
 export default function Customers() {
+  const [ nomeEmpresa, setNomeEmpresa ] = useState('')
+  const [ cnpjEmpresa, setCnpjEmpresa ] = useState('')
+  const [ enderecoEmpresa, setEnderecoEmpresa ] = useState('')
+
   return (
     <div>
-      <h1>CUSTOMERS</h1>
+      <Header />
+
+      <div className="content">
+        <Title name="Clientes">
+          <FiUser size={25} />
+        </Title>
+        
+        <div>
+          <form className="form-profile customers">
+            <label>Nome da Empresa</label>
+            <input type="text" value={nomeEmpresa} placeholder="Digite o Nome da sua Empresa" onChange={ (e) => setNomeEmpresa(e.target.value)} />
+
+            <label>CNPJ</label>
+            <input type="text" value={cnpjEmpresa} placeholder="Digite o CNPJ da sua Empresa" onChange={ (e) => setCnpjEmpresa(e.target.value)} />
+
+            <label>Endereço da Empresa</label>
+            <input type="text" value={enderecoEmpresa} placeholder="Digite o Endereço da sua Empresa" onChange={ (e) => setEnderecoEmpresa(e.target.value)} />
+
+            <button type="submit">Cadastrar Empresa</button>
+          </form>
+        </div>
+         
+      </div>
     </div>
   );
 }
