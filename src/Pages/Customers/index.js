51f24b48c377f759ae4b359a68e956452ac580e4
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-23 09:08:55 -0300

    [FEATURE] Register company on the Customers page

--- src/Pages/Customers/index.js
+++ src/Pages/Customers/index.js
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
+import firebase from "../../Services/firebaseConnection";
 
 import Header from "../../Components/Header";
 import Title from "../../Components/Title";
 
 import { FiUser } from "react-icons/fi";
+import { toast } from "react-toastify";
 
 import * as S from "./styles";
 
 export default function Customers() {
-  const [ nomeEmpresa, setNomeEmpresa ] = useState('')
-  const [ cnpjEmpresa, setCnpjEmpresa ] = useState('')
-  const [ enderecoEmpresa, setEnderecoEmpresa ] = useState('')
+  const [nomeEmpresa, setNomeEmpresa] = useState("");
+  const [cnpjEmpresa, setCnpjEmpresa] = useState("");
+  const [enderecoEmpresa, setEnderecoEmpresa] = useState("");
 
-  function handleAdd(e) {
-    e.preventDefault()
-    alert("Teste")
+  async function handleAdd(e) {
+    e.preventDefault();
+
+    if (nomeEmpresa !== "" && cnpjEmpresa !== "" && enderecoEmpresa !== "") {
+      await firebase
+        .firestore()
+        .collection("customers")
+        .add({
+          NomeEmpresa: nomeEmpresa,
+          CNPJ: cnpjEmpresa,
+          Endereço: enderecoEmpresa,
+        })
+        .then(() => {
+          setNomeEmpresa("");
+          setCnpjEmpresa("");
+          setEnderecoEmpresa("");
+          toast.info("Cadastrada com Sucesso!", {
+            theme: "colored",
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      toast.error("Preencha todos os campos!", {
+        theme: "colored",
+      });
+    }
   }
 
   return (
@@ -25,22 +52,36 @@ export default function Customers() {
         <Title name="Clientes">
           <FiUser size={25} />
         </Title>
-        
+
         <S.Container>
           <form className="form-profile customers" onSubmit={handleAdd}>
             <label>Nome da Empresa</label>
-            <input type="text" value={nomeEmpresa} placeholder="Digite o Nome da sua Empresa" onChange={ (e) => setNomeEmpresa(e.target.value)} />
+            <input
+              type="text"
+              value={nomeEmpresa}
+              placeholder="Digite o Nome da sua Empresa"
+              onChange={(e) => setNomeEmpresa(e.target.value)}
+            />
 
             <label>CNPJ</label>
-            <input type="text" value={cnpjEmpresa} placeholder="Digite o CNPJ da sua Empresa" onChange={ (e) => setCnpjEmpresa(e.target.value)} />
+            <input
+              type="text"
+              value={cnpjEmpresa}
+              placeholder="Digite o CNPJ da sua Empresa"
+              onChange={(e) => setCnpjEmpresa(e.target.value)}
+            />
 
             <label>Endereço da Empresa</label>
-            <input type="text" value={enderecoEmpresa} placeholder="Digite o Endereço da sua Empresa" onChange={ (e) => setEnderecoEmpresa(e.target.value)} />
+            <input
+              type="text"
+              value={enderecoEmpresa}
+              placeholder="Digite o Endereço da sua Empresa"
+              onChange={(e) => setEnderecoEmpresa(e.target.value)}
+            />
 
             <button type="submit">Cadastrar</button>
           </form>
         </S.Container>
-         
       </div>
     </div>
   );
