bef72e435b94e8cf173e78dfb2a44770a9b536e5
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-24 08:39:48 -0300

    [STYLE] Dashboard table

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Header from "../../Components/Header";
 import Title from "../../Components/Title";
 
-import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
 
 import * as S from "./styles";
 
@@ -30,10 +30,45 @@ export default function Dashboard() {
           </S.Container>
         ) : (
           <>
-              <Link to="/new" className="new">
-                <FiPlus size={25} color="#FFFFFF" />
-                Novo chamado
-              </Link>
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFFFFF" />
+              Novo chamado
+            </Link>
+
+            <S.Table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Sujeito</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span
+                      className="badge"
+                      style={{ backgroundColor: "#5cb85c" }}
+                    >
+                      Em aberto
+                    </span>
+                  </td>
+                  <td data-label="Cadastro">24/05/2022</td>
+                  <td data-label="#">
+                    <button className="action" style={{ backgroundColor: "#3583f6" }}>
+                      <FiSearch size={17} color="#FFFFFF" />
+                    </button>
+                    <button className="action" style={{ backgroundColor: "#f6a935" }}>
+                      <FiEdit2 size={17} color="#FFFFFF" />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </S.Table>
           </>
         )}
       </div>
--- src/Pages/Dashboard/styles.js
+++ src/Pages/Dashboard/styles.js
@@ -18,3 +18,89 @@ export const Container = styled.div`
     font-weight: 600;
   }
 `;
+
+export const Table = styled.table`
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  border: 0.1rem solid #ccc;
+  border-collapse: collapse;
+  table-layout: fixed;
+  caption {
+    font-size: 1.5em;
+    margin-block: 0.5em 0.75em;
+    @media (max-width: 700px) {
+      font-size: 1.3rem;
+    }
+  }
+  thead {
+    @media (max-width: 700px) {
+      position: absolute;
+      clip: rect(0 0 0 0);
+      height: 0.1rem;
+      width: 0.1rem;
+      margin: -0.1rem;
+      padding: 0;
+      border: none;
+      overflow: hidden;
+    }
+  }
+  tr {
+    padding: 0.35em;
+    border: 0.1rem solid #ddd;
+    background-color: #f8f8f8;
+    @media (max-width: 700px) {
+      border-bottom: 0.3rem solid #ddd;
+      display: block;
+      margin-bottom: 0.65em;
+    }
+  }
+  th,
+  td {
+    text-align: center;
+    padding: 0.62em;
+  }
+  td {
+    @media (max-width: 700px) {
+      display: block;
+      text-align: right;
+      font-size: 0.8em;
+      border-bottom: 0.1rem solid #ddd;
+    }
+  }
+  td::before {
+    @media (max-width: 700px) {
+      content: attr(data-label);
+      float: left;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+  }
+  td:last-child {
+    border-bottom: 0;
+  }
+  th {
+    font-size: 0.85em;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+  }
+  td .action {
+    display: inline-block;
+    align-items: center;
+    margin-right: 0.2rem;
+    padding: 0.5rem;
+    border: none;
+    border-radius: 0.4rem;
+  }
+  td .action svg {
+    vertical-align: middle;
+  }
+  td .badge {
+    padding: 0.3rem;
+    border-radius: 0.3rem;
+    color: #fff;
+  }
+  @media (max-width: 700px) {
+    border: none;
+  }
+`;
